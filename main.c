a6076cdded146d448f5e9ceac5f35e646cced8bf
Ubuntu <dnals0615@naver.com>
2017-10-10 02:28:54 +0900

    Add main.c

--- main.c
+++ main.c
@@ -18,7 +18,70 @@
 int main(int argc, char *argv[])
 {
 	pcap_t *handle;
+	struct pcap_pkthdr *header;
+	u_int8_t *interface = argv[1];
+	u_int8_t attacker_mac[6];
+	u_int8_t attacker_ip[4];
+	u_int8_t sender_mac[6];
+        u_int8_t sender_ip[4];
+	u_int8_t target_mac[6];
+        u_int8_t target_ip[4];
+
+
+
+
+
+	/*if(argc != 4)
+	{
+		printf("Syntax : ./send_arp <interface> <sender ip> <target ip\n>");
+		return -1;
+	}*/
+
+	handle = pcap_open_live(interface, BUFSIZ, 1, 1000, errbuf);
+	
+/*PART_1 : find attacker's ip and mac address*/
+
+/*PART_2 : make arp packet (attacker -> sender)*/
+
+/*PART_3 : get arp reply and find sender's mac address*/
+
+/*PART_4 : make arp packet (attacker -> target)*/
+ 
+/*PART_5 : get arp reply and find target's mac address*/
+
+/*PART_6 : send fake arp reply packet to sender and change sender's arp table*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
