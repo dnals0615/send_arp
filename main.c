e24f7e2564942d0581331c1be527710bbc2c9dd4
Ubuntu <dnals0615@naver.com>
2017-10-10 01:18:52 +0900

    Add main.c

--- main.c
+++ main.c
@@ -2,5 +2,23 @@
 #include <stdlib.h>
 #include <pcap.h>
 #include <string.h>
+#include <netinet/if_ether.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/ioctl.h>
+#include<net/if.h>
+#include<stdint.h>
+#include<arpa/inet.h>
+#include<netinet/in.h>
+
+#define ethernet_header_len 14
+#define ip4_header_len      20
+#define arp_header_len      28
+
+int main(int argc, char *argv[])
+{
+	pcap_t *handle;
+	
+}
 
 
